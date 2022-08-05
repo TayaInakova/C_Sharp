acf6ec4980424f26e260c3ee2c4c343f5951109a
TayaInakova <punkgrrl@yandex.ru>
2022-08-05 13:39:15 +0300

    Example009 homework

---
+++ Example009_Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int number1 = new Random().Next(1,10);
+int number2 = new Random().Next(1,10);
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+
+if (number1 > number2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(number1);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(number2);
+}
