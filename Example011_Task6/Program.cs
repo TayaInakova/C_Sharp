d8d32e5d64e6627704f7e8fddb1c4c4227d98b41
TayaInakova <punkgrrl@yandex.ru>
2022-08-09 16:01:50 +0300

    homework2.0

--- Example011_Task6/Program.cs
+++ Example011_Task6/Program.cs
@@ -1,1 +1,20 @@
-﻿// 3я домашняя задача.
+﻿void Even(int a)
+{
+    int b = a % 2;
+    if (b == 0)
+    {
+       Console.WriteLine($"{a}" + " - чётное");
+    }
+    else
+    {
+       Console.WriteLine($"{a}" + " - нечётное");
+    }
+};
+int v=4;
+int h=7;
+Even(v);
+Console.WriteLine();
+Even(-3);
+Console.WriteLine();
+Even(h);
+
--- Example012_Task8/Program.cs
+++ Example012_Task8/Program.cs
@@ -1,1 +1,19 @@
-﻿// 4я домашняя задача.
+﻿//void Even(double N)
+//{
+//    int[] numbers = {?};
+//int i=0;
+//int max=numbers[0];
+//
+//while (i<numbers.Length)
+//{
+//    if (numbers[i] > max)
+//    {
+//        max = numbers[i];
+//    };
+//    i++;
+//};
+//
+//    Console.Write("max = ");
+//    Console.WriteLine(max);
+//};
+//Even(90,5); ДОДЕЛАТЬ!
