07d38ad1af7e940d0d038060183cc592ce9e7bfb
TayaInakova <punkgrrl@yandex.ru>
2022-08-09 12:30:47 +0300

    dz

--- Example010_Task4/Program.cs
+++ Example010_Task4/Program.cs
@@ -1,1 +1,16 @@
-﻿// 2я домашняя задача.
+﻿int[] numbers = {78, 4, 56};
+Console.WriteLine($"Даны числа: {numbers[0]}, {numbers[1]}, {numbers[2]}");
+int i=0;
+int max=numbers[0];
+
+while (i<numbers.Length)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    };
+    i++;
+};
+
+    Console.Write("max = ");
+    Console.WriteLine(max);
