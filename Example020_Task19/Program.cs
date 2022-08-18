a6171c98dc334f61b6fcf159d1220e7137e051f4
TayaInakova <punkgrrl@yandex.ru>
2022-08-18 14:16:18 +0300

    Task 19 complete

---
+++ Example020_Task19/Program.cs
@@ -0,0 +1,48 @@
+﻿void Palindrome(int p)
+{
+    Console.WriteLine(p);
+
+    int k = 0, i = 0, j = 0, r = 0, c = p;
+
+    while (c > 0)
+    {
+        c /= 10;
+        k++;
+    };
+
+    int[] pal = new int[k];
+    int[] pal1 = new int[k];
+
+    for (i = 0; i < pal.Length; i++)
+    {
+        pal[i] = p % 10;
+        p = p / 10;
+    };
+
+    for (i = 0, j = 1; i < pal.Length; i++, j++)
+    {
+        pal1[i] = pal[pal.Length - j];
+    };
+
+    for (i = 0, r = 0; i < pal.Length; i++)
+    {
+        if (pal[i] == pal1[i])
+        {
+            r++;
+        };
+    };
+
+    if (r == pal.Length)
+    {
+        Console.WriteLine("Данное число - палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Данное число - не палиндром");
+    };
+
+};
+int a = 125521;
+int b = 123456;
+Palindrome(a);
+Palindrome(b);
