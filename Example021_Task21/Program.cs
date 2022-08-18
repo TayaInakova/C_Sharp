d2b29d3f3e59cef1c9873b5d139694be3518ec2b
TayaInakova <punkgrrl@yandex.ru>
2022-08-18 17:18:28 +0300

    Task 21, 23 complete

---
+++ Example021_Task21/Program.cs
@@ -0,0 +1,45 @@
+﻿double DistanceBetweenPoints()
+{
+void Point(int[] point)
+{
+    void FillArray(int[] arr)
+
+    {
+        int length = arr.Length;
+        int i = 0;
+        while (i < length)
+        {
+            arr[i] = new Random().Next(1, 20);
+            i++;
+        }
+    };
+    void PrintArray(int[] array)
+    {
+        int count = array.Length;
+        int j = 0;
+        while (j < count)
+        {
+            Console.WriteLine(array[j]);
+            j++;
+        }
+    };
+
+    FillArray(point);
+    PrintArray(point);
+    Console.WriteLine();
+};
+    int[] firstN = new int[3];
+    int[] secondN = new int[3];
+    
+    Console.WriteLine("Даны координаты первой точки:");
+Point(firstN);
+
+    Console.WriteLine("Даны координаты второй точки:");
+    Point(secondN);
+
+    double dis = Math.Sqrt(Math.Pow((secondN[0] - firstN[0]), 2) + Math.Pow((secondN[1] - firstN[1]), 2) + Math.Pow((secondN[2] - firstN[2]), 2));
+    Console.WriteLine($"Расстояние между точками: {dis}");
+    return dis;
+};
+
+DistanceBetweenPoints();
---
+++ Example022_Task23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать числа от -N до N. (Допустим, только целые)
+
+void Cube (int n)
+{
+    Console.WriteLine(n);
+    int[] num = new int[n];
+    int i = 0;
+    while (i < num.Length)
+    {
+        num[i] = (int)Math.Pow((i+1), 3);
+        Console.Write($"{num[i]} ");
+        i++;
+    };
+};
+int z = new Random().Next(1, 10);
+Cube(z);
