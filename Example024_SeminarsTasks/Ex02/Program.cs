c3ed5cbd60d5b7f79235acc87ae2698665a7c816
TayaInakova <punkgrrl@yandex.ru>
2022-08-20 00:11:57 +0300

    To be continue

---
+++ Example024_SeminarsTasks/Ex02/Program.cs
@@ -0,0 +1,90 @@
+﻿// Семинар 3.
+/*
+Задача 1.
+Сложение одномерных матриц (строка-строка)
+*/
+
+/*
+int[] Arr(int[] matrix)
+{
+    int i = 0;
+    while (i < matrix.Length)
+    {
+        matrix[i] = new Random().Next(1, 20);
+        Console.Write($"{matrix[i]} ");
+        i++;
+    }
+    Console.WriteLine();
+    return matrix;
+};
+
+int[] s1 = new int[6];
+int[] s2 = new int[6];
+
+s1 = Arr(s1);
+s2 = Arr(s2);
+
+int[] SumMatrix(int[] m1, int[] m2)
+{
+    int[] m3 = new int[m1.Length];
+    if (m1.Length != m2.Length)
+    {
+        Console.WriteLine("Матрицы должны быть однаковой размерности, замените одну или обе матрицы.");
+    }
+    else
+    {
+        for (int i = 0; i < m3.Length; i++)
+        {
+            m3[i] = m1[i] + m2[i];
+            Console.Write($"{m3[i]} ");
+        }
+        Console.WriteLine();
+    };
+    return m3;
+};
+int[] s3 = SumMatrix(s1, s2);
+*/
+
+/*
+Задача 2.
+Умножение одномерных матриц (строка-столбец) (итог - число)
+*/
+
+/*  Полная хрень...
+int[,] CreateMatrix(int x, int y)
+{
+    int[,] matrix = new int[x, y];
+    for (int i = 0; i < x; i++) //x
+    {
+        for (int j = 0; j < y; j++) //y
+        {
+            matrix[i, j] = new Random().Next(1, 20);
+        };
+    };
+    return matrix;
+};
+
+int[,] a1 = CreateMatrix(3, 1);
+int[,] a2 = CreateMatrix(1, 3);
+
+int Multiply(int[,] m1, int[,] m2)
+{
+    int m3 = 0;
+    for (int i = 0; i < m1.Length; i++)
+    {
+        m3 += m1[i, 1] + m2[1, i];
+    };
+    Console.Write($"{m3}");
+    return m3;
+};
+int a3 = Multiply(a1, a2);
+*/
+
+/*
+Задача 3.
+Умножение одномерных матриц (столбец-строка) (итог - матрица размерностью a*b)
+*/
+
+/*
+...
+*/
