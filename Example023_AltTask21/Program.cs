9ca3a9dec39ff9ec17042fa903aa379bec6f58e4
TayaInakova <punkgrrl@yandex.ru>
2022-08-21 22:58:19 +0300

    fix alt task 21

--- Example023_AltTask21/Program.cs
+++ Example023_AltTask21/Program.cs
@@ -15,7 +15,7 @@ double DistanceBetweenPoints()
     {
         Console.WriteLine("Ведите координаты через запятую:");
         string? t = Console.ReadLine();
-        t = t.Replace(" ", "");
+        t = t!.Replace(" ", "");
         string[]? tt = t.Split(",");
         int[] u = new int[tt.Length];
         for (int i = 0; i < tt.Length; i++)
