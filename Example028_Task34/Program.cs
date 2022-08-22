b4464c5554b10c93da96e8f44b338a724ccb6ab9
TayaInakova <punkgrrl@yandex.ru>
2022-08-22 17:55:20 +0300

    task 34

---
+++ Example028_Task34/Program.cs
@@ -0,0 +1,24 @@
+﻿int NumberOfEvenNumbers(int n)
+{
+    int[] Arr(int n)
+    {
+        int[] matrix = new int[n];
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            matrix[i] = new Random().Next(100, 1000);
+            Console.Write($"{matrix[i]} ");
+        }
+        Console.WriteLine();
+        return matrix;
+    };
+    int[] b = Arr(n);
+    for (int i = 0, j = 0; i < b.Length; i++)
+    {
+        if (b[i] % 10 == 0)
+        {
+            j++;
+        };
+    };
+    return j;
+};
+int g = NumberOfEvenNumbers(4);
