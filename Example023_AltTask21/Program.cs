29e71b4b1942bdd1abab210675f84f092410d148
TayaInakova <punkgrrl@yandex.ru>
2022-08-25 17:48:41 +0300

    homework 6

--- Example023_AltTask21/Program.cs
+++ Example023_AltTask21/Program.cs
@@ -1,34 +1,33 @@
 ﻿// Найти расстояние между двумя точками в пространстве по координатам
 
+int D()
+{
+    Console.WriteLine("Введите число мерности пространства:");
+    int d = Convert.ToInt32(Console.ReadLine());
+    return d;
+};
+
+int[] PointCoordinate()
+{
+    Console.WriteLine("Ведите координаты через запятую:");
+    string? t = Console.ReadLine();
+    t = t!.Replace(" ", "");
+    t = t!.Replace(".", "");
+    t = t!.Replace(";", "");
+    t = t!.Replace("/", "");
+    string[]? tt = t.Split(",");
+    int[] u = new int[tt.Length];
+    for (int i = 0; i < tt.Length; i++)
+    {
+        u[i] = Convert.ToInt32(tt[i]);
+    };
+    return u;
+};
+
 double DistanceBetweenPoints()
 {
-    int D()
-    {
-        Console.WriteLine("Введите число мерности пространства:");
-        int d = Convert.ToInt32(Console.ReadLine());
-        return d;
-    };
     int space = D();
-
-
-    int[] PointCoordinate()
-    {
-        Console.WriteLine("Ведите координаты через запятую:");
-        string? t = Console.ReadLine();
-        t = t!.Replace(" ", "");
-        t = t!.Replace(".", "");
-        t = t!.Replace(";", "");
-        t = t!.Replace("/", "");
-        string[]? tt = t.Split(",");
-        int[] u = new int[tt.Length];
-        for (int i = 0; i < tt.Length; i++)
-        {
-            u[i] = Convert.ToInt32(tt[i]);
-        };
-        return u;
-    };
     Console.WriteLine("Для первой точки:");
-
     int[] point1 = PointCoordinate();
     while (point1.Length != space)
     {
@@ -37,14 +36,12 @@ double DistanceBetweenPoints()
     };
 
     Console.WriteLine("Для второй точки:");
-
     int[] point2 = PointCoordinate();
     while (point2.Length != space)
     {
         Console.WriteLine("Число координат должно соответствовать мерности пространства, в котором находится точка. Повторите ввод:");
         point2 = PointCoordinate();
     };
-
     double distance = 0;
     for (int i = 0; i < space; i++)
     {
---
+++ Example031_Task41/Program.cs
@@ -0,0 +1,39 @@
+﻿// Допустим, пользователь не будет вводить НЕ целые числа.
+
+int[] UserInput()
+{
+    Console.WriteLine("Ведите числа через запятую:");
+    string? userText = Console.ReadLine();
+    userText = userText!.Replace(" ", "");
+    userText = userText!.Replace(";", "");
+    userText = userText!.Replace("/", "");
+    string[]? userTextArray = userText.Split(",");
+    int[] userNumberArray = new int[userTextArray.Length];
+    int size = userTextArray.Length;
+    for (int i = 0; i < size; i++)
+    {
+        userNumberArray[i] = Convert.ToInt32(userTextArray[i]);
+    };
+    return userNumberArray;
+};
+
+int QuantityGreaterThanZero(int[] array)
+{
+    int count = 0;
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+};
+
+void Start()
+{
+    int quantity = QuantityGreaterThanZero(UserInput());
+    Console.WriteLine($"Количество введенных положительных чисел: {quantity}");
+}
+Start();
